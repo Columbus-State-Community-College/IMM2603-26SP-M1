d7d300b28c5fdc028115544796f8a9a132fe7dbd
Courtney Mertz <cmertz1@student.cscc.edu>
2026-02-11 15:38:14 -0500

    Added in a pause menu to the main game scene. Also added in an exit button to the main menu scene as well.

--- M1 Granny's Cottage/Assets/Scripts/SceneLoader.cs
+++ M1 Granny's Cottage/Assets/Scripts/SceneLoader.cs
@@ -3,9 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject PauseScreen;
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void Pause()
+    {
+        PauseScreen.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPause()
+    {
+        PauseScreen.gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
