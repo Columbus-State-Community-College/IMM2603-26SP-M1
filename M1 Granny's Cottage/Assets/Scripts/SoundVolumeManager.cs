2395f097d8acca6e660e2f85c5941a8a0fee70c9
CourtneyKlug <74057284+CourtneyKlug@users.noreply.github.com>
2026-02-18 23:33:09 -0500

    Fixed up the sound options in the pause menu. Buttons become disabled if the volume is low or high enough, and you can see the float volume of the sound and music sources as well. A sound effect plays when pressing the buttons used to adjust the SFX volume.

--- M1 Granny's Cottage/Assets/Scripts/SoundVolumeManager.cs
+++ M1 Granny's Cottage/Assets/Scripts/SoundVolumeManager.cs
@@ -1,27 +1,80 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SoundVolumeManager : MonoBehaviour
 {
     public AudioSource musicAudioSource;
     public AudioSource soundEffectAudioSource;
+    public TMP_Text musicVolumeText;
+    public TMP_Text soundEffectVolumeText;
+    public AudioClip sfxTextSound;
+    public Button musicVolDownButton;
+    public Button musicVolUpButton;
+    public Button soundVolDownButton;
+    public Button soundVolUpButton;
+
+    void Start()
+    {
+        musicVolumeText.text = "Music Volume: " + musicAudioSource.volume.ToString();
+        soundEffectVolumeText.text = "SFX Volume: " + soundEffectAudioSource.volume.ToString();
+    }
 
     public void MusicVolumeDown()
     {
         musicAudioSource.volume -=0.1f;
+        musicVolumeText.text = "Music Volume: " + musicAudioSource.volume.ToString();
+
+        if(musicAudioSource.volume == 0f){
+            musicVolDownButton.interactable = false;
+        }
+
+        if(musicAudioSource.volume <= 1f){
+            musicVolUpButton.interactable = true;
+        }
     }
 
     public void MusicVolumeUp()
     {
         musicAudioSource.volume +=0.1f;
+        musicVolumeText.text = "Music Volume: " + musicAudioSource.volume.ToString();
+
+        if(musicAudioSource.volume >= 0f){
+            musicVolDownButton.interactable = true;
+        }
+
+        if (musicAudioSource.volume == 1){
+            musicVolUpButton.interactable = false;
+        }
     }
 
     public void SoundVolumeDown()
     {
         soundEffectAudioSource.volume -=0.1f;
+        soundEffectVolumeText.text = "SFX Volume: " + soundEffectAudioSource.volume.ToString();
+        soundEffectAudioSource.PlayOneShot(sfxTextSound);
+
+        if(soundEffectAudioSource.volume == 0f){
+            soundVolDownButton.interactable = false;
+        }
+
+        if(soundEffectAudioSource.volume <= 1f){
+            soundVolUpButton.interactable = true;
+        }
     }
 
     public void SoundVolumeUp()
     {
         soundEffectAudioSource.volume +=0.1f;
+        soundEffectVolumeText.text = "SFX Volume: " + soundEffectAudioSource.volume.ToString();
+        soundEffectAudioSource.PlayOneShot(sfxTextSound);
+
+        if(soundEffectAudioSource.volume >= 0f){
+            soundVolDownButton.interactable = true;
+        }
+
+        if (soundEffectAudioSource.volume == 1){
+            soundVolUpButton.interactable = false;
+        }
     }
 }
