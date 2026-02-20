85a4c572cda118270948abe4fb9ed03d41e07dc8
Courtney Mertz <cmertz1@student.cscc.edu>
2026-02-20 11:43:35 -0500

    Added core functionality to trigger the how to play screen in the main menu. The Text is a placeholder for now.

--- M1 Granny's Cottage/Assets/Scripts/SceneLoader.cs
+++ M1 Granny's Cottage/Assets/Scripts/SceneLoader.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public GameObject PauseScreen;
+    public GameObject HowToPlayScreen;
+    public TMP_Text HowToPlayButtonText;
+    public Button HowToPlayButton;
 
     public void LoadScene(string sceneName)
     {
@@ -20,6 +25,22 @@ public class SceneLoader : MonoBehaviour
     {
         PauseScreen.gameObject.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    public void HowToPlay()
+    {
+        HowToPlayScreen.gameObject.SetActive(true);
+        HowToPlayButton.onClick.AddListener(HowToPlayMenuClose);
+        HowToPlayButton.onClick.RemoveListener(HowToPlay);
+        HowToPlayButtonText.text = "BACK";
+    }
+
+    public void HowToPlayMenuClose()
+    {
+        HowToPlayScreen.gameObject.SetActive(false);
+        HowToPlayButton.onClick.RemoveListener(HowToPlayMenuClose);
+        HowToPlayButton.onClick.AddListener(HowToPlay);
+        HowToPlayButtonText.text = "HOW TO PLAY";
     }
 
     public void QuitGame()
